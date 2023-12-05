6eb30ddc787e33eb41e793b4d242cd6b63dc5d1b
Siqius <86854646+Siqius@users.noreply.github.com>
2023-12-05 11:12:29 +0100

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>MegeChomp Burger</title>
-        <link type="text/css" rel="stylesheet" href="css/style.css">
+        <link type="text/css" rel="stylesheet" href="css/admin-page.css">
     </head>
 <body>
         <header>
