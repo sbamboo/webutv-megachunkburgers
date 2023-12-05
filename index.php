99257cc27222003896c4f139b7291ae97735cf1e
Siqius <86854646+Siqius@users.noreply.github.com>
2023-12-05 11:10:51 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,67 @@
 <!DOCTYPE html>
-<html lang="sv">
+<html lang="en">
     <head>
         <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Mega Chunk Burgers</title>
+        <title>MegeChomp Burger</title>
+        <link type="text/css" rel="stylesheet" href="style.css">
     </head>
-    <body>
-    </body>
+<body>
+        <header>
+            <img src="">
+            <nav>
+                <ul>
+                    <li><a href="#">Hem</a></li>
+                    <li><a href="#">Boka</a></li>
+                    <li><a href="#">Om oss</a></li>
+                    <li><a href="#">Kontakt</a></li>
+                </ul>
+            </nav>
+        </header>
+    <main>
+        <div id="sidebar-menu">
+            <a id="menu-button">
+                Meny
+            </a>
+            <a id="book-button">
+                Boka
+            </a>
+        </div>
+        <section>
+            <div id="employees">
+                <div id="owner">
+                    <img src="">
+                    <h2>Ägare</h2>
+                    <p>Vi är ett företag som gör saker</p>
+                </div>
+                <div class="chefs chef1">
+                    <img src="">
+                    <h2>Kock 1</h2>
+                    <p>Vi är ett företag som gör saker</p>
+                </div>
+                <div class="chefs chef2">
+                    <img src="">
+                    <h2>Kock 2</h2>
+                    <p>Vi är ett företag som gör saker</p>
+                </div>
+                <div class="chefs chef3">
+                    <img src="">
+                    <h2>Kock 3</h2>
+                    <p>Vi är ett företag som gör saker</p>
+                </div>
+            </div>
+            <div id="about">
+                <h2>Om oss</h2>
+                <p>Vi är ett företag som gör saker</p>
+            </div>
+            <div id="contact">
+                <h2>Kontakt</h2>
+                <p>Vi är ett företag som gör saker</p>
+            </div>
+        </section>
+    </main>
+    <footer>
+
+    </footer>
+</body>
 </html>
