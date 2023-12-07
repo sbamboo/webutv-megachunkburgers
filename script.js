e620fb8b8cfe08a64dd4380c76900d0802a40375
Siqius <86854646+Siqius@users.noreply.github.com>
2023-12-07 14:38:30 +0100

    Update script.js
    implemented flyouts

--- script.js
+++ script.js
@@ -1,12 +1,45 @@
-// Overlap function between two sidebar buttons
-let menuButton = document.querySelector('#menu-button, #menu-button p')
-let bookButton = document.querySelector('#book-button, #book-button p')
+// Overlap function between two sidebar buttons and also make them work
+let menuButton = document.querySelector('#menu-button, #menu-button p');
+let bookButton = document.querySelector('#book-button, #book-button p');
+
+const menuPage = document.querySelector('#menu-content');
+const bookPage = document.querySelector('#book-content');
+
+let menuActive = false;
+let bookActive = false;
 
 menuButton.addEventListener('mouseover', (event) => { 
-    menuButton.style.zIndex = 1
-    bookButton.style.zIndex = 0
-},false)
+    menuButton.style.zIndex = 1;
+    bookButton.style.zIndex = 0;
+},false);
 bookButton.addEventListener('mouseover', (event) => {
-    bookButton.style.zIndex = 1
-    menuButton.style.zIndex = 0
-},false)
+    bookButton.style.zIndex = 1;
+    menuButton.style.zIndex = 0;
+},false);
+
+
+menuButton.addEventListener('mousedown', (event) => {
+    if(menuActive == true) {
+        menuActive = false;
+        menuPage.style.left = "-1500px";
+    }
+    else {
+        menuActive = true;
+        menuPage.style.left = "10%";
+        bookActive = false;
+        bookPage.style.left = "-1500px";
+    }
+},false);
+
+bookButton.addEventListener('mousedown', (event) => {
+    if(bookActive == true) {
+        bookActive = false;
+        bookPage.style.left = "-1500px";
+    }
+    else {
+        bookActive = true;
+        bookPage.style.left = "10%";
+        menuActive = false;
+        menuPage.style.left = "-1500px";
+    }
+},false);
