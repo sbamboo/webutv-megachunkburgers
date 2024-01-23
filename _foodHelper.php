cf6baca7f66261683410d358ce88bb84c781865e
Siqius <Real.FBAB@gmail.com>
2024-01-23 13:45:59 +0100

    Update _foodHelper.php

--- _foodHelper.php
+++ _foodHelper.php
@@ -2,17 +2,20 @@
 require("_functions.php");
 // SQL setup
 $sqlargs3 = array("localhost","root","","megacbur","fd_orders");
-/*
+
 $order = file_get_contents('php://input');
 $order = json_encode(json_decode($order, true));
 $order = str_replace("\u00a7","§",$order);
-*/
-$order = "Cajun_Kick§Cajun_Kick§Cajun_Kick§Cajun_Kick§Cajun_Kick§Fuego_Kick§Fuego_Kick§Salsa_Blaze§price:700§tablenr:3";
+
+//$order = "Cajun_Kick§Cajun_Kick§Cajun_Kick§Cajun_Kick§Cajun_Kick§Fuego_Kick§Fuego_Kick§Salsa_Blaze§price:700§tablenr:3";
 $tempData = parseOrderStr($order);
 $foodString = str_replace("{\"order\":\"","",str_replace("&quot;","",json_encode($tempData[0])));
 $foodString = str_replace("{","",$foodString);
 $foodString = str_replace("}","",$foodString);
 $foodString = str_replace("\"","",$foodString);
+$foodString = str_replace("order:","",$foodString);
+$foodString = str_replace("\\","",$foodString);
+echo $foodString;
 $result = saveFoodOrder($sqlargs3,$foodString,$tempData[1],$tempData[2]);
 if(str_contains($result,"successfully")) {
     echo $result;
